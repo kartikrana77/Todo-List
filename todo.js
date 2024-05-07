6a72bfdfb6dbf1c1e5b868bb960906e38534818d
kartikrana77 <kartikrana9938@gmail.com>
2024-05-07 12:48:23 +0530

    todo list completed

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
-let todoList = [];
+let e = localStorage.getItem("todolist");
+let todoList = JSON.parse(e)||[];
 displayIteam();
-
+console.log(e);
 function addTodo(){
     let inputElement = document.querySelector('#todo-input');
     let dateElement = document.querySelector('#todo-date');
@@ -10,22 +11,25 @@ function addTodo(){
     inputElement.value = '';
     dateElement.value = '';
     displayIteam();
+    localStorage.setItem("todolist", JSON.stringify(todoList));
+   
 }
 
 function displayIteam(){
-    let displayElement = document.querySelector('#display-cont');
+    let displayElement = document.querySelector('.display-cont');
    
     let newHtml = '';
     for(let i =0 ; i < todoList.length ; i++){
         let item = todoList[i].item;
         let date = todoList[i].date;
        newHtml += `
-       <div>
+       
        <span>${item}</span>
        <span>${date}</span>
-       <button onclick ="todoList.splice(${i},1);displayIteam();">Delete</button>
-       </div>
+       <button class = "b2" onclick ="todoList.splice(${i},1);displayIteam();">Delete</button>
+      
        `;
     }
     displayElement.innerHTML = newHtml;
+   
 }

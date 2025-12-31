b34939bd7927244cd1a95e4bcb6a742a51886f09
kartikrana77 <kartikrana9938@gmail.com>
2026-01-01 01:03:27 +0530

    new

--- todo.js
+++ todo.js
@@ -1,34 +1,164 @@
-let e = localStorage.getItem("todolist");
-let todoList = JSON.parse(e)||[];
-displayIteam();
+document.addEventListener('DOMContentLoaded',()=>{
+    const taskInput = document.getElementById("task-input");
+    const addTaskBtn = document.getElementById("add-task-btn");
+    const taskList = document.getElementById("task-list");
+    const emptyImage = document.querySelector('.empty-image');
+    const todosContainer = document.querySelector('.todos-container');
+    const progressBar = document.getElementById('progress');
+    const progressNumbers = document.getElementById('numbers');
 
-let addTodo = () =>{
-    let inputElement = document.querySelector('#todo-input');
-    let dateElement = document.querySelector('#todo-date');
-    let dateItem = dateElement.value;
-    let inputItem = inputElement.value;
-    todoList.push({item: inputItem,date: dateItem});
-    inputElement.value = '';
-    dateElement.value = '';
-    displayIteam();
+
+    const toggleEmptyState = () =>{
+        emptyImage.style.display = taskList.children.length === 0 ? 'block': 'none';
+        todosContainer.style.width = taskList.children.length >0? '100%': '50%';
+    }
+
+    const updateProgress = (checkCompletion = true)=>{
+        const  totalTask = taskList.children.length;
+        const completedTask = taskList.querySelectorAll('.checkbox:checked').length;
+
+        progressBar.style.width =  totalTask ? `${(completedTask/totalTask)*100}%` : '0%';
+
+        progressNumbers.textContent = `${completedTask}/ ${totalTask}`;
+
+        if(checkCompletion && totalTask > 0 && completedTask === totalTask){
+            Confetti();
+        }
+
     };
 
-    let addElement = document.querySelector('.b1');
-    addElement.addEventListener('click',addTodo);
+    const saveTasks = () => {
+  const tasks = [...taskList.children].map(li => ({
+    text: li.querySelector('span').textContent,
+    completed: li.querySelector('.checkbox').checked
+  }));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
- function displayIteam(){
-    let displayElement = document.querySelector('.display-cont');
-   
-    let newHtml = '';
-    for(let i =0 ; i < todoList.length ; i++){
-        let item = todoList[i].item;
-        let date = todoList[i].date;
-       newHtml += `
-       
-       <span>${item}</span>
-       <span>${date}</span>
-       <button id="b2"onclick="todoList.splice(${i},1);displayIteam();">Delete</button>`;  
+const loadTasks = () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.forEach(t => addTask(t.text, t.completed, false));
+  toggleEmptyState();
+  updateProgress();
+};
+
+
+
+    const  addTask = (text, completed = false,checkCompletion = true) =>{
+        const taskText= text || taskInput.value.trim();
+        if(!taskText){
+            return;
+        }
+
+        const li = document.createElement('li');
+        li.innerHTML = `<input type= "checkbox" class ="checkbox" 
+        ${completed ? 'checked': ''}/>
+        <span>${taskText}</span>
+        <div class = "task-buttons">
+        <button class = 'edit-btn'>
+        <i class ="fa-solid fa-pen"></i>
+        </button>
+
+        <button class = "delete-btn"><i
+        class="fa-solid fa-trash"></i></button>
+        </div>`;
+
+        const checkbox = li.querySelector('.checkbox');
+        const editBtn = li.querySelector('.edit-btn');
+
+        if(completed){
+            li.classList.add('completed');
+            editBtn.disabled = true;
+            editBtn.style.opacity = '0.5';
+            editBtn.style.pointerEvents = 'none';
+        }
+
+        checkbox.addEventListener("change", ()=>{
+            const isChecked = checkbox.checked;
+            li.classList.toggle('completed', isChecked);
+            editBtn.disabled= isChecked;
+            editBtn.style.opacity = isChecked ? '0.5' : '1';
+            editBtn.style.pointerEvents = isChecked ? 'none' : 'auto';
+            updateProgress();
+            saveTasks();
+        });
+
+        editBtn.addEventListener('click', ()=>{
+            if(!checkbox.checked){
+                taskInput.value = li.querySelector
+                ('span').textContent;
+                li.remove();
+                toggleEmptyState();
+                updateProgress(false);
+            }
+        });
+
+        li.querySelector('.delete-btn').addEventListener('click',()=>{
+            li.remove();
+            toggleEmptyState();
+            updateProgress();
+        });
+
+        taskList.appendChild(li);
+        taskInput.value = '';
+        toggleEmptyState();
+        updateProgress(checkCompletion);
     }
-    displayElement.innerHTML = newHtml;
-    localStorage.setItem("todolist", JSON.stringify(todoList));
+
+    addTaskBtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        addTask();
+        
+    });
+    taskInput.addEventListener('keypress', (e)=>{
+        if(e.key === 'Enter'){
+            e.preventDefault();
+            addTask();
+        }
+    })
+
+    loadTasks() 
+});
+
+
+const Confetti = ()=>{
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
 }
